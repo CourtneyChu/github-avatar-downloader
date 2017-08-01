eabb3ed9e7287ea2ea850951a9303a9d6acb4206
Courtney Chu <CourtneyHChu@gmail.com>
2017-08-01 04:51:45 +0000

    final commit

--- download_avatars.js
+++ download_avatars.js
@@ -5,26 +5,34 @@ var GITHUB_TOKEN = "584633432f7a860f774f7985496713f559c5d124";
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var t = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'CourtneyChu'
+    }
+  };
 
-var t = {
-  url: requestURL,
-  headers: {
-    'User-Agent': 'CourtneyChu'
-  }
-};
-
-request(t, function(err, response, body) {
-  if (err) throw err;
-  console.log('Response Status Code:', response.statusCode)
-  var parsed = JSON.parse(body);
-  cb(err, parsed);
-  })
-
-};
+  request(t, function(err, response, body) {
+    if (err) { throw err; }
+    console.log('Response Status Code:', response.statusCode);
+    var parsed = JSON.parse(body);
+    cb(err, parsed);
+  });
+}
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+var fs = require('fs');
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function(err) {
+      console.log(err);
+    })
+    .pipe(fs.createWriteStream(filePath));
+}
+
+getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
 //  console.log("Errors:", err);
 //  console.log("Result:", result);
   result.forEach(function(item) {
@@ -35,13 +43,4 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 
 });
 
-var fs = require('fs');
-
-function downloadImageByURL(url, filePath) {
- request.get(url)
-  .on('error', function(err) {
-    console.log(err)
-    })
-    .pipe(fs.createWriteStream(filePath));
-  }
 
