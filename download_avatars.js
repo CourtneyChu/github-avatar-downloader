09a2033a2ee18838ccbf79c068e72f0761d09c8a
Courtney Chu <CourtneyHChu@gmail.com>
2017-08-01 01:02:52 +0000

    creating the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,10 @@
 var request = require('request');
+var GITHUB_USER = "CourtneyChu";
+var GITHUB_TOKEN = "584633432f7a860f774f7985496713f559c5d124";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL)
 }
 
 
