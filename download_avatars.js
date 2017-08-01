ffcc59eb98829c1126393caa3ac9d475f9f1b727
Courtney Chu <CourtneyHChu@gmail.com>
2017-08-01 02:10:41 +0000

    added user agent

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,29 @@ var request = require('request');
 var GITHUB_USER = "CourtneyChu";
 var GITHUB_TOKEN = "584633432f7a860f774f7985496713f559c5d124";
 
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-console.log(requestURL)
+
+var t = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'Courtney'
+  }
+};
+
+request(t, function(err, response, body) {
+  if (err) throw err;
+  console.log('Response Status Code:', response.statusCode)
+  console.log(body)
+});
+
 }
+
+
+
+
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
